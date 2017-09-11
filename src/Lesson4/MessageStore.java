8804dace02b15cf908d998e11057e24eca06aab0
aparc <a.parchukov@yandex.ru>
2017-09-11 15:53:02 +0300

    new

---
+++ src/Lesson4/MessageStore.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class MessageStore {
+
+    static Message[] array = new Message[1];
+
+    public static void add(Message message){
+        array[0] = message;
+    }
+
+
+}
