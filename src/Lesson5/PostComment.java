2cdedce5306f54db69b895ad48c2c4b5ead8fc24
aparc <a.parchukov@yandex.ru>
2017-09-13 22:44:44 +0300

    Initial commit

---
+++ src/Lesson5/PostComment.java
@@ -0,0 +1,17 @@
+package Lesson5;
+
+import Lesson3.MainPost.Post;
+
+public class PostComment extends AbstractMessage {
+
+    Post post;
+
+    PostComment(){
+
+    }
+
+    @Override
+    public void send() {
+
+    }
+}
