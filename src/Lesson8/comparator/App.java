3998fc95643120ffbe3b594a9bf5f5dddeddbed8
aparc <a.parchukov@yandex.ru>
2017-10-02 15:34:40 +0300

    commit

--- src/Lesson8/comparator/App.java
+++ src/Lesson8/comparator/App.java
@@ -2,6 +2,7 @@ package Lesson8.comparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
@@ -18,7 +19,13 @@ public class App {
         list.add(new User("krot", "QWE324"));
         list.add(new User("kitty", "qwerty"));
 
-        Collections.sort(list, new UserLoginLengthComparator().thenComparing(new UserLoginAlphabetComparator()));
+//        Collections.sort(list, new UserLoginLengthComparator().thenComparing(new UserLoginAlphabetComparator()));
+
+        list.sort((o1, o2) -> {
+            if(o2.getLogin().length() < o1.getLogin().length()) return -1;
+            else if(o2.getLogin().length() > o1.getLogin().length()) return 1;
+            else if(o2.getLogin().length() - o1.getLogin().length() == 0);
+                return o1.getLogin().compareTo(o2.getLogin());});
 
         for(User x: list){
             System.out.println(x.toString());
